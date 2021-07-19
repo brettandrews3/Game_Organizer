615076d274c74edf1e560de35ecaaf1c1aac955a
Brett Andrews <brettandrews3@gmail.com>
2021-07-19 16:42:35 -0500

    First commit. Has dictionary key/value pairs for video game descriptors. Next: test the approach, how it saves data

---
+++ game_organizer.py
@@ -0,0 +1,20 @@
+"""
+I'm wanting to write a simple program that will catalog my Switch games.
+This may end up being based on class inheritance, dictionaries, lists,
+or something else. I'll figure it out as I go along.
+"""
+
+# Let's try a dictionary first to define the Switch game's components:
+
+game = {
+        'Title': 'Name of Game',
+        'Publisher': 'Name of Publisher',
+        'Format': 'Physical | Digital',
+        'Genre': 'Adventure | Platformer | Metroidvania | RPG | Shooter',
+        'Own': 'Yes | No',
+        'Played': 'Yes | No',
+        'Active': 'Yes | No',
+        'Finished': 'Yes | No',
+        'Review': '1 - 5'
+}
+print(game)
