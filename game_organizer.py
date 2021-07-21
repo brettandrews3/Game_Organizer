fe818be8698b793b6d0fb8cdc023b69d388c3187
Brett Andrews <brettandrews3@gmail.com>
2021-07-21 17:37:13 -0500

    Commented out the dictionary I drafted. Wrote the game_feature() and list of default questions, answers.

--- game_organizer.py
+++ game_organizer.py
@@ -19,3 +19,24 @@ game = {
 }
 print(game)
 #TODO: Try adding a game to this list.
+
+# Here, I'm defining a function that can scale with the questions needed for this organizer:
+def game_feature(query, default):
+    question = query + ' [' + default + ']? '
+    answer = input(question)
+    if (answer == ''):
+        answer = default
+    print(f'You chose {answer}.')
+    return answer
+
+# Here's my list of game attribute questions.
+
+title = game_feature('What is the game\'s title', 'None')
+publisher = game_feature('Who is the publisher', 'indie')
+game_format = game_feature('Is this game physical or digital', 'digital')
+genre = game_feature('What genre would you give this,', "I don't know")
+own = game_feature('Do you currently own this game,', 'Yes')
+played = game_feature('Have you played this game yet,', 'No')
+active = game_feature('Are you actively playing this game', 'No')
+finished = game_feature('Did you complete the game?', 'No')
+review = game_feature('How would you rate this game(1-5)', '4')
